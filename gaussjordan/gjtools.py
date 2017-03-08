6ea0480ff54844dde67739a65ec56c091b2bfa35
Ali <ali.abbas786872@gmail.com>
2017-03-08 11:49:36 -0500

    lo and behold

--- gaussjordan/gjtools.py
+++ gaussjordan/gjtools.py
@@ -12,3 +12,28 @@ def mul_row(m, k, ri):
 def add_row(m, k, ri, rj):
     for i in range(len(m[ri-1])):
         m[rj-1][i] += k * m[ri-1][i]
+
+def reduced_row_echelon(m):
+    """Tranform augmented matrix m to reduced row echelon form."""
+
+    from fractions import Fraction 
+    # Set first value in row 1 to 1
+    mul_row(m, Fraction(1, m[0][0]), 1)
+
+    # multiply row 1 by the first element of row 2 and add it to row 2
+    add_row(m, -m[1][0], 1, 2)
+
+    add_row(m, -m[2][0], 1, 3)
+
+    mul_row(m, Fraction(1, m[1][1]), 2)
+
+    add_row(m, -m[2][1], 2, 3)
+
+    mul_row(m, Fraction(1, m[2][2]), 3)
+
+    add_row(m, -m[1][2], 3, 2)
+
+    add_row(m, -m[0][2], 3, 1)
+
+    add_row(m, -m[0][1], 2, 1)
+    print_matrix(m)
