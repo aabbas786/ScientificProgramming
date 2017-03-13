4f6847f220dd3ff5349f5e32d5e5ad45363b8789
Ali <ali.abbas786872@gmail.com>
2017-03-13 14:07:56 -0400

    Added test and removed white spaces

--- gaussjordan/gjtools.py
+++ gaussjordan/gjtools.py
@@ -1,17 +1,18 @@
 def print_matrix(m):
     for row in m:
-        print('|{0:6s}{1:6s}{2:6s}| {3:6s}|'.format(*[str(val) for val in row]))
+        print('|{0:6s}{1:6s}{2:6s}|{3:6s}|'.format(*[str(val) for val in row]))
     print()
 
 
 def mul_row(m, k, ri):
     for i in range(len(m[ri-1])):
-        m[ri-1][i] *= k 
+        m[ri-1][i] *= k
 
 
 def add_row(m, k, ri, rj):
     for i in range(len(m[ri-1])):
         m[rj-1][i] += k * m[ri-1][i]
+
 
 def reduced_row_echelon(m):
     """Tranform augmented matrix m to reduced row echelon form."""
@@ -37,3 +38,7 @@ def reduced_row_echelon(m):
 
     add_row(m, -m[0][1], 2, 1)
     print_matrix(m)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testfile("gjtest")
