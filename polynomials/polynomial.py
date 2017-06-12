b514ee9a1740c8dade9283a318749c04b5b115fd
Ali Abbas <aabbas@ebgranville.gctaa.net>
2017-06-12 12:30:22 -0400

    bell rang commited broken add polys

--- polynomials/polynomial.py
+++ polynomials/polynomial.py
@@ -7,107 +7,128 @@ class Polynomial:
       >>> p.coeffs
       (3, 4, 1)
     """
+    """
+      >>> p.poly_to_str()
+      '3x^2 + 4x + 1'
+    """
     def __init__(self, coeffs=(0,)):
         self.coeffs = coeffs
 
-'''
-def get_opp(p, i):
-    """
-      >>> get_opp((7, -3, 5, -6), 1)
-      ' - '
-      >>> get_opp((7, -3, 5, -6), 2)
-      ' + '
-      >>> get_opp((7, -3, 5, -6), 3)
-      ' - '
-      >>> get_opp((7, -3, 5, -6), 0)
-      ''
-      >>> get_opp((-3, 5, -6), 0)
-      '-'
-    """
-    if i == 0:
-        if p[0] > 0:
-            return ''
+
+    def get_opp(self, i):
+        """
+          >>> p = Polynomial((7, -3, 5, -6))
+          >>> p.get_opp(1)
+          ' - '
+          >>> p.get_opp(2)
+          ' + '
+          >>> p.get_opp(3)
+          ' - '
+          >>> p.get_opp(0)
+          ''
+          >>> p2 = Polynomial((-3, 5, -6))
+          >>> p2.get_opp(0)
+          '-'
+        """
+        if i == 0:
+            if self.coeffs[0] > 0:
+                return ''
+            else:
+                return '-'
+        if self.coeffs[i] > 0:
+            return ' + '
         else:
-            return '-'
-    if p[i] > 0:
-        return ' + '
-    else:
-        return ' - '
+            return ' - '
 
 
-def get_xterm(p, i):
-    """
-      >>> get_xterm((8, 6, 7, 9), 0)
-      'x^3'
-      >>> get_xterm((1, 8, 6, 7, 9), 2)
-      'x^2'
-      >>> get_xterm((1, 8, 6, 7, 9), 0)
-      'x^4'
-      >>> get_xterm((8, 6, 7, 9), 2)
-      'x'
-      >>> get_xterm((8, 6, 7, 9), 3)
-      ''
-    """
-    exp = len(p) - (i + 1)
-    if exp == 1:
-        return 'x'
-    if exp == 0:
-        return ''
-    return 'x^{0}'.format(exp)
-    
+    def get_xterm(self, i):
+        """
 
-def poly_to_str(p):
-    """
-      >>> poly_to_str((4, 3, 2))
-      '4x^2 + 3x + 2'
-      >>> poly_to_str((2, 8, 3))
-      '2x^2 + 8x + 3'
-      >>> poly_to_str((8, 6, 7, 9))
-      '8x^3 + 6x^2 + 7x + 9'
-      >>> poly_to_str((7, -3, 5, -6))
-      '7x^3 - 3x^2 + 5x - 6'
-      >>> poly_to_str((5, 0, 2))
-      '5x^2 + 2'
-      >>> poly_to_str((-7, 0, 3, 5, 0, -2))
-      '-7x^5 + 3x^3 + 5x^2 - 2'
-      >>> poly_to_str((-7, 0, 0, 5, 0, 0))
-      '-7x^5 + 5x^2'
-    """
-    polystr = ''
-
-    for i in range(len(p)):
-        coeff = abs(p[i])
-        if coeff == 0:
-            continue
-        xterm = get_xterm(p, i)
-        opp = get_opp(p, i)
-        polystr += '{0}{1}{2}'.format(opp, coeff, xterm)
-
-    return polystr
+          >>> p = Polynomial((8, 6, 7, 9))
+          >>> p.get_xterm(0)
+          'x^3'
+          >>> p.get_xterm(2)
+          'x'
+          >>> p.get_xterm(3)
+          ''
+          >>> p2 = Polynomial((1, 8, 6, 7, 9))
+          >>> p2.get_xterm(2)
+          'x^2'
+          >>> p2.get_xterm(0)
+          'x^4'
+        """
+        exp = len(self.coeffs) - (i + 1)
+        if exp == 1:
+            return 'x'
+        if exp == 0:
+            return ''
+        return 'x^{0}'.format(exp)
 
 
-def add_polys(p1, p2):
-    """
-      >>> add_polys((3, 1, 2), (1, 2, 3))
-      (4, 3, 5)
-      >>> poly_to_str(add_polys((4, 3, 5, 9), (2, -3, 2, -9)))
-      '6x^3 + 7x'
-      >>> add_polys((5, 3, 1, 2), (1, 2, 3))
-      (5, 4, 3, 5)
-      >>> add_polys((5, 3), (1, 2, 3, 4))
-      (1, 2, 8, 7)
-    """
-    if len(p1) < len(p2):
-        p1 = ((0, ) * (len(p2) - len(p1))) + p1
-    elif len(p2) < len(p1):
-        p2 = ((0, ) * (len(p1) - len(p2))) + p2
-    p3 = []
-    for i in range(len(p1)):
-        p3.append(p1[i] + p2[i])
-    
-    return tuple(p3)
+    def __repr__(self):
+        """
+          >>> p = Polynomial((4, 3, 2))
+          >>> p
+          4x^2 + 3x + 2
+          >>> p2 = Polynomial((2, 8, 3))
+          >>> print(p2)
+          2x^2 + 8x + 3
+          >>> p3 = Polynomial((8, 6, 7, 9))
+          >>> p3
+          8x^3 + 6x^2 + 7x + 9
+          >>> p4 = Polynomial((7, -3, 5, -6))
+          >>> p4
+          7x^3 - 3x^2 + 5x - 6
+          >>> p5 = Polynomial((5, 0, 2))
+          >>> print(p5)
+          5x^2 + 2
+          >>> p6 = Polynomial((-7, 0, 3, 5, 0, -2))
+          >>> p6
+          -7x^5 + 3x^3 + 5x^2 - 2
+          >>> p7 = Polynomial((-7, 0, 0, 5, 0, 0))
+          >>> p7
+          -7x^5 + 5x^2
+        """
+        polystr = ''
+
+        for i in range(len(self.coeffs)):
+            coeff = abs(self.coeffs[i])
+            if coeff == 0:
+                continue
+            xterm = self.get_xterm(i)
+            opp = self.get_opp(i)
+            polystr += '{0}{1}{2}'.format(opp, coeff, xterm)
+
+        return polystr
 
 
+    def add_polys(self, other):
+        """
+          >>> p1 = Polynomial((3, 1, 2))
+          >>> p2 = Polynomial((1, 2, 3))
+          >>> p1.add_polys(p2)
+          4x^2 + 3x + 5
+          >>> p3 = Polynomial((4, 3, 5, 9))
+          >>> p4 = Polynomial((2, -3, 2, -9))
+          >>> p3.add_polys(p4).coeffs
+          (6, 0, 7, 0)
+          >>> p5 = Polynomial((5, 3, 1, 2))
+          >>> p6 = Polynomial((1, 2, 3))
+          >>> p5.add_polys(p6).coeffs
+          (5, 4, 3, 5)
+        """
+        if len(self.coeffs) < len(other.coeffs):
+            self.coeffs = ((0, ) * (len(other.coeffs) - len(self.coeffs))) +
+                          self.coeffs
+        elif len(other.coeffs) < len(self.coeffs):
+            p2 = ((0, ) * (len(p1) - len(p2))) + p2
+        p3 = []
+        for i in range(len(p1)):
+            p3.append(p1[i] + p2[i])
+        
+        return tuple(p3)
+
+'''
 def term_x_poly(coeff, exp, p):
     """
       >>> term_x_poly(4, 2, (3, 0, 2, 0, 0, 0))
